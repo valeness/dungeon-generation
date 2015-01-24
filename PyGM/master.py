87a8219e32fa2770c333320bb72ea1c7bc8213d4
James <valeness84@gmail.com>
2015-01-23 23:06:04 -0600

    Initial Commit

---
+++ PyGM/master.py
@@ -0,0 +1,194 @@
+#Game Maker Object
+from math import sqrt
+import pygame
+
+### Utility Functions ###
+# Concise way to load images to pygame
+def load_image(name):
+    image = pygame.image.load(name)
+    return image
+
+# Minimize code needed to draw sprite group
+def group_draw(group_name, screen):
+    group_name.update()
+    group_name.draw(screen)
+
+def draw_rect(screen, color, obj, dimensions):
+    width = dimensions[0]
+    height = dimensions[1]
+    rect = (obj.x, obj.y, width, height)
+    pygame.draw.rect(screen, color, rect)
+
+
+class Entity(object):
+    #all game classes inherit this class
+    def __init__(self, x=0, y=0, myid=-1, type_id=-1):
+        self.x = x
+        self.y = y
+        self.myid = myid
+        self.type_id = type_id
+        self.is_destroyed = False
+        self.deactivated_id = -1
+
+        self.alarm = {}
+
+    def is_activated(self):
+        return self.deactivated_id == -1
+
+    def destroy(self):
+        self.is_destroyed = True
+
+    def event_step(self):
+
+        #alarm stuff
+        #usage: self.alarm_dictionary["MethodName"] = StepsToComplete
+        #does not support arguments at this current time.
+        if len(self.alarm) > 0:
+            #make a list of alarms which ran out
+            alarms_to_remove = []
+
+            for method_alarm in self.alarm:
+                #count down the timer
+                self.alarm[method_alarm] -= 1
+
+                #if the timer has run out
+                if self.alarm[method_alarm] < 0:
+
+                    #dynamic alarms. Alarms names are actually method names
+                    if hasattr(self, method_alarm):
+                        perform = getattr(self, method_alarm)
+                        perform()
+                    alarms_to_remove.append(method_alarm)
+            for method_alarm in alarms_to_remove:
+                self.alarm.pop(method_alarm)
+
+    def event_draw(self):
+        pass
+
+class Create_Sprite(pygame.sprite.Sprite):
+    def __init__(self, sprimage, obj):
+        super(Create_Sprite, self).__init__()
+        self.images = []
+        self.images.append(load_image(sprimage))
+
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = pygame.Rect(obj.x, obj.y, 62, 75)
+
+    def update(self):
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index]
+
+class Draw(pygame.sprite.Sprite):
+    def __init__(self, drimage, obj):
+        super(Draw, self).__init__()
+        pass
+
+class NewGameRoom:
+    object_dictionary = {}
+    object_type_dict = {}
+    object_id_counter = 0
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def room_step():
+        for obj in NewGameRoom.object_dictionary.values():
+            obj.event_step()
+
+    #this method will create a type list when we actually need it
+    #some classes will never get a type list.
+    @staticmethod
+    def add_object_type_list(name):
+        if not name.__name__ in NewGameRoom.object_type_dict:
+            #create the list
+            NewGameRoom.object_type_dict[name.__name__] = []
+
+            #iterate through all objects to add to the list
+            for obj in NewGameRoom.object_dictionary.values():
+                if obj.__class__.__name__ == name.__name__:
+                    obj.type_id = len(NewGameRoom.object_type_dict[name.__name__])
+                    NewGameRoom.object_type_dict[name.__name__].append(obj.myid)
+
+    @staticmethod
+    def add_object(x, y, obj):
+        #find the id to assign to object
+        assign_id = NewGameRoom.object_id_counter
+        NewGameRoom.object_id_counter += 1
+        assign_type_id = -1
+
+        #we only will add our type if this object requires such a thing
+        #this is for performance issues that may come later on.
+        #a "tree" or "rock" may not need to be iterated through for calculations
+        if obj.__name__ in NewGameRoom.object_type_dict:
+            #we tell the type list where to find us in the object_list
+            NewGameRoom.object_type_dict[obj.__name__].append(assign_id)
+
+            #find the type id or sub id
+            assign_type_id = len(NewGameRoom.object_type_dict[obj.__name__])
+
+        #finally we create the object
+        NewGameRoom.object_dictionary[assign_id] = obj(x=x, y=y, myid=assign_id, type_id=assign_type_id)
+
+    @staticmethod
+    def object_nearest(x, y):
+        tracking_distance = -1
+        object_id = -1
+        for obj in NewGameRoom.object_dictionary.values():
+            distance = sqrt((x-obj.x)**2+(y-obj.y)**2)
+            if tracking_distance == -1 or distance < tracking_distance:
+                tracking_distance = distance
+                object_id = obj.myid
+        return object_id
+
+    @staticmethod
+    def type_nearest(x, y, mtype):
+        #if we have not iterated through this type
+        #we must create the list for this type
+        if not mtype.__name__ in NewGameRoom.object_type_dict:
+            NewGameRoom.add_object_type_list(mtype)
+
+        tracking_distance = -1
+        object_id = -1
+        for value in NewGameRoom.object_type_dict[mtype.__name__]:
+            #remember object_index(id) references the object_list
+            obj = NewGameRoom.object_index(value)
+            distance = sqrt((x-obj.x)**2+(y-obj.y)**2)
+            if tracking_distance == -1 or distance < tracking_distance:
+                tracking_distance = distance
+                object_id = obj.myid
+        return object_id
+
+    @staticmethod
+    def object_index(position):
+        #CAUTION! Should be used carefully.
+        #if not used properly you could
+        #leave an object in the room without
+        #being freeing it when it should be
+        #freed
+        if position != -1:
+            if position in NewGameRoom.object_dictionary:
+                return NewGameRoom.object_dictionary[position]
+
+    @staticmethod
+    def instance_destroy(destroy_id):
+        #if we are given an instance instead of an id
+        #we will find the id to delete
+        if isinstance(destroy_id, Entity):
+            destroy_id = destroy_id.myid
+
+        instance_to_destroy = NewGameRoom.object_dictionary[destroy_id]
+        instance_to_destroy.destroy()
+
+        if instance_to_destroy.__class__.__name__ in NewGameRoom.object_type_dict:
+            #if killing this instance will delete the list
+            if len(NewGameRoom.object_type_dict[instance_to_destroy.__class__.__name__]) <= 1:
+                NewGameRoom.object_type_dict.pop(instance_to_destroy.__class__.__name__)
+            else:
+                #if there will be remaining objects we wont delete the list but remove the entry.
+                NewGameRoom.object_type_dict[instance_to_destroy.__class__.__name__].pop(instance_to_destroy.type_id)
+
+        NewGameRoom.object_dictionary.pop(destroy_id)
---
+++ main.py
@@ -0,0 +1,119 @@
+import sys, time
+from PyGM.master import *
+import math
+from random import choice, randrange
+
+pygame.display.init()
+
+pygame.font.init()
+mono_font = pygame.font.SysFont("monospace", 12)
+
+screen = pygame.display.set_mode((640, 480))
+FPS = 15
+clock = pygame.time.Clock()
+
+# Rectanlge Coords
+ROOM = (64, 64)
+HALL = (32, 16)
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+running = True
+pygame.display.set_caption("Dungeon Generator")
+
+color_list = [RED, WHITE, GREEN, BLUE]
+
+class GameTimer(Entity):
+	def __init__(self, **kw):
+		super(GameTimer, self).__init__(**kw)
+
+	def event_step(self):
+		super(GameTimer, self).event_step()
+
+class Room(Entity):
+	def __init__(self, **kw):
+		super(Room, self).__init__(**kw)
+
+	def draw(self):
+		draw_rect(screen, WHITE, self, ROOM)
+
+class Hall(Entity):
+	def __init__(self, **kw):
+		super(Hall, self).__init__(**kw)
+
+	def draw(self):
+		draw_rect(screen, WHITE, self, HALL)
+
+size = 16
+
+NewGameRoom()
+NewGameRoom.add_object(0, 0, GameTimer)
+NewGameRoom.add_object(128, 128, Room)
+
+start_room = NewGameRoom.object_index(1)
+
+struct_list = ["room", "hall"]
+room_list = []
+
+def generate(cellsX, cellsY, cellSize=72):
+
+	class Cell(object):
+		def __init__(self, x, y, id, type):
+			self.x = x
+			self.y = y 
+			self.id = id
+			self.type = type
+
+	cells = {}
+	last_x = 0
+	last_y = 0
+
+	for y in range (cellsY):
+		for x in range(cellsX):
+			cell_id = len(cells) + 1
+			c = Cell(x, y, cell_id)
+			cells[((c.x * cellSize), (c.y * cellSize), c.id)] = c
+
+	for i in cells:
+		choice = randrange(1, 3)
+		x = i[0]
+		y = i[1]
+		id = i[0]
+		if choice == 2:
+			pygame.draw.rect(screen, RED, (x, y, cellSize, cellSize))
+			num = mono_font.render(str(id), 1, BLUE)
+			screen.blit(num, (x, y))
+			last_x = x
+			last_y = y
+		elif last_x == x:
+			last_x = x
+			pygame.draw.rect(screen, RED, (x, y, cellSize, cellSize))
+			num = mono_font.render(str(id), 1, BLUE)
+			screen.blit(num, (x, y))
+		elif last_y == y:
+			last_y = y
+			pygame.draw.rect(screen, RED, (x, y, cellSize, cellSize))
+			num = mono_font.render(str(id), 1, BLUE)
+			screen.blit(num, (x, y))
+		else:
+			pass
+
+
+generate(20, 15, 32)
+
+while running:
+	keys = pygame.key.get_pressed()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT or (keys[pygame.K_ESCAPE]):
+			running = False
+
+	#start_room.draw()
+
+	NewGameRoom.room_step()
+	clock.tick(FPS)
+
+	pygame.display.flip()
